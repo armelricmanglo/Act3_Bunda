ff539f9285717d3750a77ddb4a31ae3ce6e205e2
Armel Ric Manglo <armelricmanglo@gmail.com>
2021-06-21 15:52:33 +0800

    Initial commit

---
+++ app/src/main/java/com/example/act3_bunda/login_sucess.java
@@ -0,0 +1,14 @@
+package com.example.act3_bunda;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class login_sucess extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_sucess);
+    }
+}
